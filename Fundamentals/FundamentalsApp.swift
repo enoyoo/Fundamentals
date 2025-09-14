44b572e82027e45ddd37230c18f54264e6709f9b
Eno <enoyoo02@gmail.com>
2025-09-14 13:50:11 -0400

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Eno Yoo on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
